0b0bc41f7e1c342577daa1a9aa5bb5272bce8750
zablon <zablonoigo51@gmail.com>
2023-11-27 21:28:14 +0300

    js file

---
+++ theme/static/js/main.js
@@ -0,0 +1,12 @@
+
+let images=document.querySelectorAll()
+let span=document.querySelectorAll()
+let currentImg=0 //Index of the first image
+const interval=3000 //duration speed of the slider
+
+
+function changeSlide(n){
+    for(let i=0; i<images.length; i++){
+        
+    }
+}
