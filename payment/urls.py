9c02cd4b2604d99acf8da08a3732967950aed92b
zablon <zablonoigo51@gmail.com>
2023-11-27 21:26:00 +0300

    payment app

---
+++ payment/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (payment_process,payment_completed,payment_canceled)
+
+app_name='payment'
+
+urlpatterns=[
+    path('process/', payment_process, name='process'),
+    path('completed/', payment_completed, name='completed'),
+    path('canceled/', payment_canceled, name='canceled'),
+]
