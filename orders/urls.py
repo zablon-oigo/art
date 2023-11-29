d67e3acd7128c8acace5d5a6daf2f01ec8226040
zablon <zablonoigo51@gmail.com>
2023-11-29 21:30:36 +0300

    webhook path

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import (order_create)
+from .views import (order_create, admin_order_detail)
 
 app_name='orders'
 
 urlpatterns=[
     path('create/', order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name='admin_order_detail'),
 ]
