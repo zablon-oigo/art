b168f6dba8660fc46272c91da140aee357a3d708
zablon <zablonoigo51@gmail.com>
2023-12-07 10:23:47 +0300

    script file

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,20 @@
 
-// let slides = document.getElementsByClassName("image");
-// let slideIndex = 0;
-// showSlide();
-
-// function showSlide() {
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.display = 'none';
-//     }
-
-//     slideIndex++;
-//     if (slideIndex > slides.length) {
-//         slideIndex = 1;
-//     }
-
-//     slides[slideIndex - 1].style.display = 'block';
-//     setTimeout(showSlide, 3000);
-// }
 
 
-const mycards=document.getElementsByClassName("card")
-let cardIndex=0
+const testimonials = document.getElementsByClassName("testimonials");
+let cardIndex = 0;
 showCard();
-function showCard(){
-  
 
-    for (let i = 0; i < mycards.length; i++) {
-        mycards[i].classList.add('hidden');
-        
+function showCard() {
+    for (let i = 0; i < testimonials.length; i++) {
+        testimonials[i].classList.add('hidden');
     }
+
     cardIndex++;
-    if (cardIndex > mycards.length) {
+    if (cardIndex > testimonials.length) {
         cardIndex = 1;
     }
-    mycards[cardIndex - 1].classList.remove('hidden');
+
+    testimonials[cardIndex - 1].classList.remove('hidden');
     setTimeout(showCard, 3000);
 }
