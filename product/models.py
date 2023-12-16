0f3dee986213fb37dcf132dc8e52ee70bcc3e3b6
zablon <zablonoigo51@gmail.com>
2023-12-16 07:18:33 +0300

    extra image field

--- product/models.py
+++ product/models.py
@@ -39,6 +39,9 @@ class Product(models.Model):
     name=models.CharField(max_length=200)
     slug=models.SlugField(max_length=200)
     image=models.ImageField(upload_to='products/%Y/%m/%d')
+    image_1=models.ImageField(upload_to='products',blank=True, null=True)
+    image_2=models.ImageField(upload_to='products',blank=True, null=True)
+    image_3=models.ImageField(upload_to='products',blank=True, null=True)
     description=models.TextField(blank=True)
     price=models.DecimalField(max_digits=10, decimal_places=2)
     available=models.BooleanField(default=True)
@@ -99,4 +102,13 @@ class Exhibition(models.Model):
 
 
 
+class Testimonial(models.Model):
+    name=models.CharField(max_length=65)
+    designation=models.CharField(max_length=65, null=True, blank=True)
+    image=models.ImageField(upload_to='testimonials/%Y/%m/%d')
+    description=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.name}'
