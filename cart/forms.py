7e9dd699723b192faf8025b6442ce29a2488fc8d
zablon <zablonoigo51@gmail.com>
2023-12-19 22:36:13 +0300

    add to cart forms

--- cart/forms.py
+++ cart/forms.py
@@ -3,8 +3,10 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES=[(i, str(i)) for i in range(1, 4)]
 
 class CartAddProductForm(forms.Form):
-    quantity=forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,label='', coerce=int,widget=forms.Select(attrs={
-        'class':''
-    }))
+    #quantity=forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,label='', coerce=int,widget=forms.Select(attrs={
+        #'class':''
+    # }))
+     # Adding a field to track the action of adding to the cart
+    add_to_cart = forms.BooleanField(initial=True, widget=forms.HiddenInput())
     override=forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput())
     
