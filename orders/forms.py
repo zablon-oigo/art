a3e533a56283aa8004413f6e054e0a360608d408
zablon <zablonoigo51@gmail.com>
2024-02-28 10:50:32 +0300

    change input width

--- orders/forms.py
+++ orders/forms.py
@@ -7,11 +7,11 @@ class OrderCreateForm(forms.ModelForm):
         model=Order
         fields=['first_name','last_name','email','address','postal_code','city']
         widgets={
-            'first_name':forms.TextInput(attrs={'class':'px-6 py-4 rounded-lg border-2 border-gray-300 focus:outline-none'}),
-            'last_name':forms.TextInput(attrs={'class':'px-6 py-4 rounded-lg border-2 border-gray-300 focus:outline-none'}),
-            'email':forms.EmailInput(attrs={'class':'px-6 py-4 rounded-lg border-2 border-gray-300 focus:outline-none'}),
-            'address':forms.TextInput(attrs={'class':'px-6 py-4 rounded-lg border-2 border-gray-300 focus:outline-none'}),
-            'postal_code':forms.TextInput(attrs={'class':'px-6 py-4 rounded-lg border-2 border-gray-300 focus:outline-none'}),
-            'city':forms.TextInput(attrs={'class':'px-6 py-4 rounded-lg border-2 border-gray-300 focus:outline-none'}),
+            'first_name':forms.TextInput(attrs={'class':'w-full px-10 py-2 rounded-xl  focus:outline-none'}),
+            'last_name':forms.TextInput(attrs={'class':'w-full px-10 py-2 rounded-xl  focus:outline-none'}),
+            'email':forms.EmailInput(attrs={'class':'w-full px-10 py-2 rounded-xl  focus:outline-none'}),
+            'address':forms.TextInput(attrs={'class':'w-full px-10 py-2 rounded-xl  focus:outline-none'}),
+            'postal_code':forms.TextInput(attrs={'class':'w-full px-10 py-2 rounded-xl  focus:outline-none'}),
+            'city':forms.TextInput(attrs={'class':'w-full px-10 py-2 rounded-xl  focus:outline-none'}),
         }
         
