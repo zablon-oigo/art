b913e3d6ba6c10f45215e3df168ffe5db6de759e
zablon <zablonoigo51@gmail.com>
2024-03-02 21:48:38 +0300

    Changed nodels apperance on admin site

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, Exhibition,ArtSize,Testimonial
+from .models import Category, Product,ArtSize
 
 
 admin.site.register(ArtSize)
@@ -18,18 +18,3 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter=['available','created','updated']
     list_editable=['price','available']
     prepopulated_fields={'slug':('name',)}
-    
-
-
-@admin.register(Exhibition)
-class ExhibitionAdmin(admin.ModelAdmin):
-    list_display=['title','venue','price','start_time','end_time','date']
-    list_editable=['start_time','end_time','date']
-    list_filter=['publish',]
-
-
-
-@admin.register(Testimonial)
-class TestimonialAdmin(admin.ModelAdmin):
-    list_display=['name','created']
-    list_filter=['created',]
