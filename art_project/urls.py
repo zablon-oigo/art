2f74aebee5a802a564ca8ab8b3a2905fef6a9470
zablon <zablonoigo51@gmail.com>
2024-03-03 16:30:45 +0300

    Tailwind automatic browser reload

--- art_project/urls.py
+++ art_project/urls.py
@@ -26,10 +26,10 @@ urlpatterns = [
     path('orders/',include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
     path('', include('product.urls', namespace='product')),
+     path("__reload__/", include("django_browser_reload.urls")),
    
     
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIR)
