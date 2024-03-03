3c9e8edca5cb68aaab47973807cdb5e503a9e9a6
zablon <zablonoigo51@gmail.com>
2024-03-03 16:30:10 +0300

    browser reload

--- art_project/settings.py
+++ art_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'payment.apps.PaymentConfig',
     'tailwind',
     'theme',
+    'django_browser_reload'
 ]
 TAILWIND_APP_NAME='theme'
 
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'art_project.urls'
@@ -134,13 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL='media/'
-MEDIA_ROOT=BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
