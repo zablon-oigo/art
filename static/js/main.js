9cb67b0b9662039487e03f9996915adedc895883
zablon <zablonoigo51@gmail.com>
2024-03-15 14:07:59 +0300

    script for smaller screen menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,15 @@
-
+innitApp=()=>{
+    const menuBtn =document.querySelector('#menuBtn')
+    const menu =document.querySelector('#menu')
+   
+       const toggleMenu=()=>{
+           menu.classList.toggle('hidden')
+           menu.classList.toggle('flex')
+   
+   
+       }
+       menuBtn.addEventListener('click',toggleMenu)
+   
+   }
+   document.addEventListener('DOMContentLoaded',innitApp)
 
