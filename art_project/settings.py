491dbaab50ef9eedade9804595860df7afccb931
zablon <zablonoigo51@gmail.com>
2024-04-12 20:30:35 +0300

    Removed Tailwind configuration

--- art_project/settings.py
+++ art_project/settings.py
@@ -64,6 +64,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'art_project.urls'
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
 
 TEMPLATES = [
     {
